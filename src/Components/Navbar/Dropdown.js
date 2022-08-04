3a77573ca03301c410b9ed97f60b397d9486e708
Francine <franmelopepe2@gmail.com>
2022-08-04 16:26:06 +0200

    navbar done

---
+++ src/Components/Navbar/Dropdown.js
@@ -0,0 +1,97 @@
+import {
+  Drawer,
+  DrawerBody,
+  //   DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  // RadioGroup,
+  // Radio,
+  // Stack,
+  Button,
+  useDisclosure,
+} from "@chakra-ui/react";
+
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import styles from "./Dropdown.module.css";
+import open_menu from "../../Images/shared/icon-hamburger.svg";
+import close_menu from "../../Images/shared/icon-close.svg";
+import Navbar from "./Navbar";
+
+export default function Dropdown() {
+  const navLinks = [
+    {
+      number: "00",
+      name: "Home",
+      link: "/home",
+    },
+    {
+      number: "01",
+      name: "Destination",
+      link: "/destination",
+    },
+    {
+      number: "02",
+      name: "Crew",
+      link: "/crew",
+    },
+    {
+      number: "03",
+      name: "Technology",
+      link: "/technology",
+    },
+  ];
+
+  const drawerStyle = {
+    backgroundColor: "rgba(255, 255, 255, 0.05)",
+  };
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [placement, setPlacement] = useState("right");
+
+  return (
+    <>
+      <Button
+        mr="2.5rem"
+        colorScheme="transparent"
+        onClick={onOpen}
+        className={styles.open_button}
+        // style={{ display: isOpen ? "block" : "none" }}
+      >
+        <img src={open_menu} alt="" />
+      </Button>
+      <Drawer placement={placement} onClose={onClose} isOpen={isOpen}>
+        <DrawerOverlay />
+        <DrawerContent
+          bgColor={"rgba(255, 255, 255, 0.05)"}
+          backdropFilter="auto"
+          backdropBlur="20px"
+        >
+          <DrawerBody>
+            <Button
+              marginTop={3}
+              colorScheme="transparent"
+              paddingLeft="15rem"
+              onClick={onClose}
+              className={styles.close_button}
+            >
+              <img src={close_menu} alt="" />
+            </Button>
+
+            <nav className={styles.nav_container}>
+              <ul className={styles.nav_link_mobile}>
+                {navLinks.map((navLinks) => (
+                  <li className={styles.list_container}>
+                    <NavLink to={navLinks.link} className={styles.active}>
+                      <span>{navLinks.number}</span> {navLinks.name}
+                    </NavLink>
+                  </li>
+                ))}
+              </ul>
+            </nav>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+}
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,36 +1,53 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import styles from "./Navbar.module.css";
+import Logo from "../../Images/shared/logo.svg";
+// import icon_hamburger from "../../Images/shared/icon-hamburger.svg";
+import Dropdown from "./Dropdown";
 
 const Navbar = () => {
+  const navLinks = [
+    {
+      number: "00",
+      name: "Home",
+      link: "/home",
+    },
+    {
+      number: "01",
+      name: "Destination",
+      link: "/destination",
+    },
+    {
+      number: "02",
+      name: "Crew",
+      link: "/crew",
+    },
+    {
+      number: "03",
+      name: "Technology",
+      link: "/technology",
+    },
+  ];
+
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <NavLink
-              to="/home"
-              className={({ isActive }) => (isActive ? styles.active : "")}
-            >Home</NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/destination"
-              className={({ isActive }) => (isActive ? styles.active : "")}
-            >Destination</NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/crew"
-              className={({ isActive }) => (isActive ? styles.active : "")}
-            >Crew</NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/technology"
-              className={({ isActive }) => (isActive ? styles.active : "")}
-            >Technology</NavLink>
-          </li>
+      <nav className={styles.nav_container}>
+        <NavLink to="/home">
+          <img src={Logo} alt="space_logo" />
+        </NavLink>
+        <label htmlFor="toggler">
+          <Dropdown />
+        </label>
+
+        <hr />
+        <ul className={styles.nav_link}>
+          {navLinks.map((navLinks) => (
+            <li className={styles.list_container}>
+              <NavLink to={navLinks.link} className={styles.active}>
+                <span>{navLinks.number}</span> {navLinks.name}
+              </NavLink>
+            </li>
+          ))}
         </ul>
       </nav>
     </>
